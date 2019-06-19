a3205f8a3e2671a5febbc951b01b069a72a23916
pupuzuimei <2977759369@qq.com>
2019-06-19 19:59:19 +0800

    Initial Commit

---
+++ test_6.19/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test_6.19
+//
+//  Created by s20181106278 on 2019/6/19.
+//  Copyright © 2019 s20181106278. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
